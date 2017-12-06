ef0510442dc9c7fe99de0f0965534adde68c28a2
kilowat <kadantsev.sergey@gmail.com>
2017-12-06 12:38:34 +0300

    init

--- intellectservice.charts/install/gadgets/charts_gadget/lang/ru/.description.php
+++ intellectservice.charts/install/gadgets/charts_gadget/lang/ru/.description.php
@@ -1,3 +1,5 @@
 <?php
 $MESS["GD_INSERVES_CHART_MODULE_NAME"] = "Конверсия заказов";
 $MESS["GD_INSERVES_CHART_MODULE_DESCR"] = "Конверсия заказов в диаграммах";
+$MESS["intellectservice.charts_PARTNER_NAME"] = "Интеллект-сервис";
+$MESS["intellectservice.charts_PARTNER_URI"] = " https://intellect-service.pro";
