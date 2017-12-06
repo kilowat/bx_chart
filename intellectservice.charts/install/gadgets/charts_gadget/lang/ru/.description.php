e371eb2505d1dc300854b564553e0eac066af6fb
kilowat <kadantsev.sergey@gmail.com>
2017-12-06 15:19:33 +0300

    init

--- intellectservice.charts/install/gadgets/charts_gadget/lang/ru/.description.php
+++ intellectservice.charts/install/gadgets/charts_gadget/lang/ru/.description.php
@@ -1,5 +1,5 @@
 <?php
-$MESS["GD_INSERVES_CHART_MODULE_NAME"] = "Конверсия заказов";
-$MESS["GD_INSERVES_CHART_MODULE_DESCR"] = "Конверсия заказов в диаграммах";
+$MESS["GD_INSERVES_CHART_MODULE_NAME"] = "Диаграммы заказов";
+$MESS["GD_INSERVES_CHART_MODULE_DESCR"] = "Гаджет диаграммы заказов";
 $MESS["intellectservice.charts_PARTNER_NAME"] = "Интеллект-сервис";
 $MESS["intellectservice.charts_PARTNER_URI"] = " https://intellect-service.pro";
--- intellectservice.charts/lang/ru/install/index.php
+++ intellectservice.charts/lang/ru/install/index.php
@@ -1,6 +1,6 @@
 <?
-$MESS["intellectservice.charts_MODULE_NAME"] = "Графики продаж";
-$MESS["intellectservice.charts_MODULE_DESC"] = "Графики продаж";
+$MESS["intellectservice.charts_MODULE_NAME"] = "Диаграммы заказов";
+$MESS["intellectservice.charts_MODULE_DESC"] = "Гаджет диаграммы заказов";
 $MESS["intellectservice.charts_PARTNER_NAME"] = "Интеллект-сервис";
 $MESS["intellectservice.charts_PARTNER_URI"] = "https://intellect-service.pro";
 ?>
